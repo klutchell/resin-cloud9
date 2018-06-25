2a6eac8f1defade27e0411f649c9183a76abded2
Kyle Harding <kylemharding@gmail.com>
2018-06-25 15:49:11 +0000

    disable docker on startup

--- cloud9/start.sh
+++ cloud9/start.sh
@@ -5,7 +5,7 @@
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # start docker daemon in the background
-/usr/bin/dockerd --iptables=false --dns 8.8.8.8 --storage-driver aufs &
+# /usr/bin/dockerd --iptables=false --dns 8.8.8.8 --storage-driver aufs &
 
 # start cloud9 server in the foreground
 node server.js -p ${C9_PORT} -w ${C9_WORKSPACE} -l 0.0.0.0 -a :
