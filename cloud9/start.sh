d6c98354452b45948018e871df5774aa1b9db326
Kyle Harding <kylemharding@gmail.com>
2018-06-28 08:36:39 -0400

    use cloud9 image from docker repo

--- cloud9/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# set timezone with TZ
-# eg. TZ=America/Toronto
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-
-# start cloud9 server in the foreground
-node server.js -p ${C9_PORT} -w ${C9_WORKSPACE} -l 0.0.0.0 -a ${C9_USER}:${C9_PASS}
